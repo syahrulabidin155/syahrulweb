0112fdf5d36a78990162f3fa74b8b409fd18f406
syahrul <syahrulabidin155@gmail.com>
2024-07-20 22:16:12 +0700

    perubahan

--- auth/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo "<script>alert('Berhasil logout dari SIM-BPS'); window.location='../index.php';</script>";
--- templates/footer.php
+++
@@ -1,8 +0,0 @@
-</div>   
-  </div>
-  <!-- end header -->
-<!-- JavaScript -->
-<script src="assets/plugins/jquery/jquery.min.js"></script>
-<script src="assets/plugins/bootstrap4/js/bootstrap.min.js"></script>
-</body>
-</html>
